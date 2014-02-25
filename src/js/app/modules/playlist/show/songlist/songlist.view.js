f89767a60f0f634e97ce69748f9763780d893d01
eabait <estebanabait@gmail.com>
2014-02-24 22:48:39 -0300

    Keyup handler at document level to close the offcanvas when user presses Esc key

--- src/js/app/modules/playlist/show/songlist/songlist.view.js
+++ src/js/app/modules/playlist/show/songlist/songlist.view.js
@@ -1,26 +1,47 @@
 define(
   [
+    'underscore',
+    'jquery',
     'marionette',
     './song.view',
     './song.empty.view'
   ],
-  function(Marionette, SongView, SongEmptyView) {
+  function(_, $, Marionette, SongView, SongEmptyView) {
     'use strict';
 
     var SongListView = Marionette.CompositeView.extend({
-      template: "songlist.hbs",
+      template: 'songlist.hbs',
+
       itemView: SongView,
-      itemViewContainer: "div",
+
+      itemViewContainer: 'div',
+
       className: 'full-height',
+
       emptyView: SongEmptyView,
 
       events: {
         'click .js-add-song': 'onClickAddSong'
       },
 
+      initialize: function() {
+        _.bindAll(this, 'onKeyUp');
+        $(document).keyup(this.onKeyUp);
+      },
+
+      onKeyUp: function(e) {
+        if (e.which === 27) {
+          $('.toggle-input').attr('checked', false);
+        }
+      },
+
       onClickAddSong: function(e) {
         e.preventDefault();
         this.trigger('playlist:addsong');
+      },
+
+      onClose: function() {
+        $(document).unbind('keyup', this.onKeyUp);
       }
 
     });
