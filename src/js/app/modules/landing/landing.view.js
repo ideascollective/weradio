f17ac4d7f7f4b8bfd00cdf7bfb370407739363a9
eabait <estebanabait@gmail.com>
2014-03-05 11:04:52 -0300

    Typeahead autocomplete component integration

--- src/js/app/modules/landing/landing.view.js
+++ src/js/app/modules/landing/landing.view.js
@@ -1,9 +1,13 @@
 define(
   [
+    'jquery',
     'backbone',
-    'marionette'
+    'marionette',
+    'playlistcollection',
+    'bloodhound',
+    'typeahead'
   ],
-  function(Backbone, Marionette) {
+  function($, Backbone, Marionette, PlaylistCollection, Bloodhound, typeahead) {
     'use strict';
 
     var LandingView = Marionette.ItemView.extend({
@@ -15,10 +19,31 @@ define(
         'click .js-create-playlist': 'onCreatePlaylistClick'
       },
 
+      onRender: function() {
+        var playlists = new Bloodhound({
+          datumTokenizer: function(d) {
+            return Bloodhound.tokenizers.whitespace(d.name);
+          },
+          queryTokenizer: Bloodhound.tokenizers.whitespace,
+          local: PlaylistCollection.toJSON()
+        });
+
+        playlists.initialize();
+
+        this.$el.find('.js-autocomplete').typeahead(null, {
+          displayKey: 'name',
+          source: playlists.ttAdapter()
+        });
+      },
+
       onCreatePlaylistClick: function(e) {
         e.preventDefault();
         this.trigger('playlist:create');
       },
+
+      onClose: function() {
+        $('.js-autocomplete').typeahead('destroy');
+      }
     });
 
     return LandingView;
