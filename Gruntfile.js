590c2bc4ced1b7758ff66810682d27a6704473b1
Franco Monsalvo <franco.monsalvo@gmail.com>
2014-03-15 15:15:13 -0300

    pushing crap to the repo!

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,7 +76,7 @@ module.exports = function(grunt) {
 
     concat: {
       css: {
-        src: [PATH_ASSETS_CSS + '/*.css'],
+        src: ['src/vendor/normalize-css/normalize.css', PATH_ASSETS_CSS + '/*.css'],
         dest: PATH_DEPLOY_ASSETS +
           '/css/<%= pkg.name %>-<%= pkg.version %>.concat.css'
       }
